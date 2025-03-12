3e6e811a4e9baea078ff6aa4238a7128f8d864b3
root <root@PC3>
2025-03-12 20:36:04 +0000

    deploy

--- main.py
+++ main.py
@@ -83,4 +83,6 @@ async def predict_bottleneck(input_data: HardwareInput):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
